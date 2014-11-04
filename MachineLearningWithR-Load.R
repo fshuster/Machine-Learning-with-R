b15578b1a6275b20e0563c6e1ec23ccba57ec841
fshuster <fcshuster@gmail.com>
2014-11-03 22:53:09 -0500

    Initial commit of Load file for R Packages for Machine Learning with R

---
+++ MachineLearningWithR-Load.R
@@ -0,0 +1,9 @@
+# Set working Directory
+
+setwd("C:/Data/MachineLearningWithR")
+
+# Chapter 2 Packages
+
+library("gmodels", lib.loc="C:/Users/fshuster/Documents/R/win-library/3.0")
+
+# Chapter 3 Packages
